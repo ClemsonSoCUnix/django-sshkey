1f6deb03cd30c9f3f619ac73e3a1b8f60dee8aec
Scott Duckworth <sduckwo@clemson.edu>
2013-06-12 10:43:18 -0400

    make sure curl is silent

--- lookup.sh
+++ lookup.sh
@@ -4,10 +4,10 @@ url="$1"
 if [ $# -eq 1 ]; then
   read line
   fingerprint=$(ssh-keygen -lf /dev/stdin <<< $line | cut -f2 -d' ')
-  exec curl -G "$url" --data-urlencode "fingerprint=${fingerprint}"
+  exec curl -s -G "$url" --data-urlencode "fingerprint=${fingerprint}"
 elif [ $# -eq 2 ]; then
   username="$2"
-  exec curl -G "$url" --data-urlencode "username=${username}"
+  exec curl -s -G "$url" --data-urlencode "username=${username}"
 else
   echo "Invalid number of arguments" >&2
   exit 2
