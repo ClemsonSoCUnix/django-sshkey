a7871041efb88b8e98164b26682151a332702841
Scott Duckworth <sduckwo@clemson.edu>
2013-07-03 14:00:55 -0400

    display user, name, and fingerprint in admin

--- sshkey/admin.py
+++ sshkey/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from sshkey.models import UserKey
 
 class UserKeyAdmin(admin.ModelAdmin):
+  list_display = [
+    '__unicode__',
+    'user',
+    'name',
+    'fingerprint',
+  ]
   search_fields = [
     'user__username',
   ]
