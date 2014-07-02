abae8ecdd5f178a07f9a2cae55dc733336b87898
Scott Duckworth <sduckwo@clemson.edu>
2014-07-02 13:15:34 -0400

    customize UserKeyForm widgets with placeholder text

--- django_sshkey/forms.py
+++ django_sshkey/forms.py
@@ -33,3 +33,14 @@ class UserKeyForm(forms.ModelForm):
   class Meta:
     model = UserKey
     fields = ['name', 'key']
+    widgets = {
+      'name': forms.TextInput(attrs={
+        'size': 50,
+        'placeholder': "username@hostname, or leave blank to use key comment",
+      }),
+      'key': forms.Textarea(attrs={
+        'cols': 72,
+        'rows': 15,
+        'placeholder': "Paste in the contents of your public key file here",
+      }),
+    }
