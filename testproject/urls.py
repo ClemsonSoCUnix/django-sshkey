5691a6db3b8229c4d22e31528effe70b0721f954
Scott Duckworth <sduckwo@clemson.edu>
2014-07-29 16:00:09 -0400

    add missing url from django 1.6 compat

--- testproject/urls.py
+++ testproject/urls.py
@@ -7,9 +7,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^', include('django_sshkey.urls')),
+  url(r'^admin/', include(admin.site.urls)),
 )
