1732b29291c5616579ef1c6b363a1c41a250edaf
Scott Duckworth <sduckwo@clemson.edu>
2014-07-29 15:14:46 -0400

    make compatible with django 1.3 - 1.6

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,9 +1,15 @@
-from django.conf.urls.defaults import patterns, include, url
+try:
+  from django.conf.urls import patterns, include, url
+except ImportError:
+  from django.conf.urls.defaults import patterns, include, url
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-  url(r'^', include('django_sshkey.urls')),
-  url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-  url(r'^admin/', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'testproject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
 )
