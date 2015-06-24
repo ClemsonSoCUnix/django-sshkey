91f889f7a5cf3ba10f5f6fe968062a05b5a2c0fc
Paul Kilgo <pkilgo@clemson.edu>
2015-06-24 13:28:12 -0400

    test against multiple django versions with tox

--- testproject/settings.py
+++ testproject/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import django
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,9 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'django_sshkey',
 )
+
+if django.VERSION < (1, 7):
+    INSTALLED_APPS += ('south',)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
